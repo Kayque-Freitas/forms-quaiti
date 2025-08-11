fc95f2796ef8d19cd0759a35782ecd51b48c690d
Kayque-Freitas <kayquefreitasxd85@gmail.com>
2025-08-11 20:31:45 -0300

    formulario quaiar

---
+++ calcular.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = $_POST ["nome"];
+$idade = $_POST ["idade"];
+$peso = $_POST ["peso"];
+$altura = $_POST ["altura"];
+$sexo = $_POST ["sexo"];
+
+$imc = ($altura * $altura) / $peso;
+
+if ()
